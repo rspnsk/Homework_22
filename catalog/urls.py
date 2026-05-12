854765b572354f33592c6fe4515f79e35ff29282
Yuri <rspnsk@mail.ru>
2026-05-13 01:58:34 +0300

    'init'

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, contacts_us
+from catalog.views import home, contacts, contacts_us, product_detail, product_list, base, add_product
 
 app_name = CatalogConfig.name
 
@@ -8,4 +8,8 @@ urlpatterns = [
     path('home/', home, name='home'),
     path('contacts/', contacts, name='contacts'),
     path('send-message/', contacts_us, name='contacts-us'),
+    path('product_list/', product_list, name='product_list'),
+    path('base/', base, name='base'),
+    path('product_detail/<int:product_id>', product_detail, name='product_detail'),
+    path('add_product/', add_product, name='add_product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Product, ContactInfo
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product, ContactInfo
+from django.shortcuts import render, redirect
+from .forms import ProductForm
 
 
 def home(request):
@@ -9,21 +11,45 @@ def home(request):
 
     # Выводим их в консоль
     for product in latest_products:
-        print(f"{product.name} - {product.price} руб.")  # или другой вывод, который вам нужен
+        print(f"{product.name} - {product.price} руб.")  # или другой вывод, который нам нужен
     context = {'latest_products': latest_products}
-    return render(request, 'home.html', context)
+    return render(request, 'catalog/home.html', context)
 
 def contacts_us(request):
     if request.method == 'POST':
         # Получение данных из формы
         name = request.POST.get('name')
         message = request.POST.get('message')
-        # Обработка данных (например, сохранение в БД, отправка email и т. д.)
         # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
-    return render(request, 'contacts.html')
+    return render(request, 'catalog/contacts.html')
 
 
 def contacts(request):
     contacts = ContactInfo.objects.all()
-    return render(request, 'contacts.html', {'contacts': contacts})
+    return render(request, 'catalog/contacts.html', {'contacts': contacts})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/product_list.html', context)
+
+def base(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/base.html', context)
+
+def add_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('catalog:product_list')
+    else:
+        form = ProductForm()
+    return render(request, 'catalog/add_product.html', {'form': form})
